e13044919d3fa5b2179bab41d760749d319b64e8
MDarshan123 <72216152+MDarshan123@users.noreply.github.com>
2020-10-03 16:07:56 +0530

    first commit

---
+++ secondfile.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello world"
+
+# @app.route("/Ds")
+# def ds():
+#     return "Hello Darshan23"
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    name="dsmalaviya"
+    return render_template("about.html",name2=name)
+
+@app.route("/bootstrap")
+def boot():
+    return render_template("bootstrap.html")
+
+app.run(debug=True)
